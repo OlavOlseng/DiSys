6b87e9098292c25190fcbd984f59cfa89e73e1a8
eirikjak <eirikjak@gmail.com>
2014-03-18 12:13:41 +0100

    code for p4

--- p1/src/test/HelloWorldClientServer.java
+++ p1/src/test/HelloWorldClientServer.java
@@ -1,28 +1,39 @@
 package test;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 
 public class HelloWorldClientServer {
 
 	public static void main(String[] args) {
 		
-		System.setSecurityManager(new RMISecurityManager());
+		System.setProperty("java.security.policy","file:///home/.../<filename>.policy");
 		if(args.length > 0){
 			
 			ConntectToServer(args[0]);
 			
 		}else{
 			
-			ListenForClient("127.0.0.1:1337");
+			ListenForClient("127.0.0.1");
+		}
+		
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
 	public static void ConntectToServer(String addr){
+		System.out.println("helldakjsdhaksjalsd");
 		String url = "rmi://" + addr + "/HelloWorld";
+		System.out.println(url);
 		try {
 			HelloWorld helloWorld = (HelloWorld)Naming.lookup(url);
 			
@@ -38,10 +49,17 @@ public class HelloWorldClientServer {
 			e.printStackTrace();
 		}
 		
+		
+		
 	}
 	
 	public static void ListenForClient(String localAddr){
-		
+		try {
+			LocateRegistry.createRegistry(1099);
+		} catch (RemoteException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		HelloWorld helloWorld = null;
 		try {
 			helloWorld = new HelloWorldImpl();
